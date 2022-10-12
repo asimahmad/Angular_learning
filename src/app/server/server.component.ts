35b7f82055491030d87b35b48b555b26fea4cb81
Shakeel, Asim <Asim.Shakeel@bshg.com>
2022-10-12 15:37:32 +0530

    template with inline css

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html',
+    // templateUrl: './server.component.html',
+    template: `
+    <h3 style="color:red">
+    Hey this is from the template 
+    </h3>`,
     // styleUrls:['./server.component.css']
     styles:[` 
     h3{
