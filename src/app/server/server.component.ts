8e355e67ec50c914b22f447cee8026bcd0eddd21
Shakeel, Asim <Asim.Shakeel@bshg.com>
2022-10-13 14:10:37 +0530

    Selector with element and class selector to use within angualr

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,18 +1,29 @@
 import { Component } from "@angular/core";
 
+/* 
+* green comments
+? boolean comment
+*/
+
 @Component({
-    selector: 'app-server',
-    // templateUrl: './server.component.html',
-    template: `
-    <h3 style="color:red">
-    Hey this is from the template 
-    </h3>`,
+    selector: 'app-server', 
+    // selector: '[app-server]',  with using element
+    // selector:'.app-server', using class selector
+    templateUrl: './server.component.html',
+    // template: `
+    // <h3 style="color:red">
+    // Hey this is from the template 
+    // </h3>`,
     // styleUrls:['./server.component.css']
     styles:[` 
     h3{
         color:blue
-    }
-    `]
+    }`,
+    `
+    h3{
+        background-color:yellow
+    }`
+    ]
 })
 export class ServerComponent{
 
